cf6c784c9e614f3c52707fef083a9b3a574a8a34
t3kt <tommy.etkin@gmail.com>
2015-03-28 20:33:36 -0400

    updated the usage text and added a flag for disabling console value displays

--- myo-osc.cpp
+++ myo-osc.cpp
@@ -247,11 +247,19 @@ enum optionIndex {
   POSE,
   EMG,
   SYNC,
+  CONSOLE,
   HELP
 };
 enum OptionType {DISABLE, ENABLE, OTHER};
+const char usageText[] =
+"USAGE: myo-osc [options] <host> <port>\n"
+"   Myo-OSC sends OSC output over UDP from the input of a Thalmic Myo armband.\n"
+"   IP address defaults to 127.0.0.1/localhost\n"
+"   Port defaults to 7777\n"
+"   by Samy Kamkar -- http://samy.pl -- code@samy.pl\n"
+"   modified by tekt -- https://t3kt.net\n";
 const option::Descriptor usage[] = {
-  {UNKNOWN, 0, "", "", option::Arg::None, "USAGE: myo-osc [options] <host> <port>\n\n"},
+  {UNKNOWN, 0, "", "", option::Arg::None, usageText},
   {ACCEL, ENABLE, "a", "accel", option::Arg::None, "--accel Enable accelerometer output"},
   {ACCEL, DISABLE, "A", "noaccel", option::Arg::None, "--noaccel Disable accelerometer output"},
   {GYRO, ENABLE, "g", "gyro", option::Arg::None, "--gyro Enable gyroscope output"},
@@ -264,6 +272,7 @@ const option::Descriptor usage[] = {
   {EMG, DISABLE, "E", "noemg", option::Arg::None, "--noemg Disable EMG output"},
   {SYNC, ENABLE, "s", "sync", option::Arg::None, "--emg Enable sync/unsync output"},
   {SYNC, DISABLE, "S", "nosync", option::Arg::None, "--noemg Disable sync/unsync output"},
+  {CONSOLE, DISABLE, "C", "noconsole", option::Arg::None, "--noconsole Disable console value display"},
   {HELP, 0, "", "help", option::Arg::None, "--help Print usage and exit."},
   {0, 0, 0, 0, 0, 0},
 };
@@ -280,7 +289,7 @@ bool parseArgs(int argc, char **argv, Settings* settings) {
   
   if (options[HELP] && argc == 0) {
     option::printUsage(std::cout, usage);
-    exit(0);
+    return false;
   }
   
   settings->port = 7777;
@@ -328,8 +337,12 @@ bool parseArgs(int argc, char **argv, Settings* settings) {
       case SYNC:
         settings->sync = opt.type() == ENABLE;
         break;
+      case CONSOLE:
+        settings->console = opt.type() == ENABLE;
+        break;
       case UNKNOWN:
-        std::cout << "Unknown option: " << std::string(opt.name, opt.namelen) << "\n";
+        std::cout << "Unknown option: " << std::string(opt.name, opt.namelen) << "\n\n";
+        option::printUsage(std::cout, usage);
         return false;
       default:
         break;
@@ -341,7 +354,8 @@ bool parseArgs(int argc, char **argv, Settings* settings) {
   } else if (parse.nonOptionsCount() == 1) {
     settings->port = atoi(parse.nonOption(0));
   } else {
-    std::cout << "strange number of non-option arguments: " << parse.nonOptionsCount() << "\n";
+    std::cout << "strange number of non-option arguments: " << parse.nonOptionsCount() << "\n\n";
+    option::printUsage(std::cout, usage);
     return false;
   }
   return true;
@@ -353,36 +367,11 @@ int main(int argc, char** argv)
   // We catch any exceptions that might occur below -- see the catch statement for more details.
   try
   {
-//    if (argc != 3 && argc != 2 && argc != 1)
-//    {
-//      std::cout << "\nusage: " << argv[0] << " [IP address] <port>\n\n" <<
-//      "Myo-OSC sends OSC output over UDP from the input of a Thalmic Myo armband.\n" <<
-//      "IP address defaults to 127.0.0.1/localhost\n\n" <<
-//      "by Samy Kamkar -- http://samy.pl -- code@samy.pl\n";
-//      exit(0);
-//    }
     if (!parseArgs(argc, argv, &settings))
       return 1;
     
-    std::cout << settings;
+//    std::cout << settings;
     
-//    if (argc == 1)
-//    {
-//    }
-//    else if (argc == 2)
-//    {
-//      settings.port = atoi(argv[1]);
-//    }
-//    else if (argc == 3)
-//    {
-//      settings.hostname = argv[1];
-//      settings.port = atoi(argv[2]);
-//    }
-//    else
-//    {
-//      std::cout << "well this awkward -- weird argc: " << argc << "\n";
-//      exit(0);
-//    }
     std::cout << "Sending Myo OSC to " << settings.hostname << ":" << settings.port << "\n";
     
     
